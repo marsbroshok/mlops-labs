bd6f641ccaae4580f7812929db99fdeed22a8375
jarokaz <jarokaz@no-reply.com>
2019-12-05 03:40:01 +0000

    Updates to lab 11

---
+++ Lab-11-TFDV/01-Covertype-Dataset/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    name='custom-predictor-2',
+    description='Custom prediction routine.',
+    version='0.1',
+    install_requires=[
+      'httplib2==0.12.0',
+      'tensorflow==1.15',
+      'tensorflow-serving-api==1.15',
+      'tfx==0.15'
+    ]
+)
