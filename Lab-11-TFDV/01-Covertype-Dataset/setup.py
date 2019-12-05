9f0326c15c1d36554281b8342004a8e07574fa05
jarokaz <jarokaz>
2019-12-05 04:24:30 +0000

    Updates to lab 11

--- Lab-11-TFDV/01-Covertype-Dataset/setup.py
+++ Lab-11-TFDV/01-Covertype-Dataset/setup.py
@@ -6,9 +6,9 @@ setup(
     description='Custom prediction routine.',
     version='0.1',
     install_requires=[
-      'httplib2==0.12.0',
-      'tensorflow==1.15',
-      'tensorflow-serving-api==1.15',
-      'tfx==0.15'
+ #     'httplib2==0.12.0',
+ #     'tensorflow==1.15',
+ #     'tensorflow-serving-api==1.15',
+      'tensorflow_data_validation==0.15.0'
     ]
 )
