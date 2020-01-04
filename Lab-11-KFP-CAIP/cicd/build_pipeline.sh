d4ba247cb136b0acbf6bd2e063d8dba8083676c2
Jarek Kazmierczak <jarokaz@hotmail.com>
2020-01-03 21:34:12 -0800

    Update build_pipeline.sh

--- Lab-11-KFP-CAIP/cicd/build_pipeline.sh
+++ Lab-11-KFP-CAIP/cicd/build_pipeline.sh
@@ -17,9 +17,10 @@
 # the pipelines and pipelines components 
 
 SUBSTITUTIONS=\
+_CLUSTER_NAME=[YOUR_ENDPOINT],\
 _TRAINER_IMAGE_NAME=trainer_image,\
 _BASE_IMAGE_NAME=base_image,\
-TAG_NAME=latest,\
+TAG_NAME=manual,\
 _PIPELINE_FOLDER=Lab-11-KFP-CAIP/pipelines,\
 _PIPELINE_DSL=covertype_training_pipeline.py,\
 _PIPELINE_PACKAGE=covertype_training_pipeline.yaml,\
@@ -27,7 +28,6 @@ _PIPELINE_NAME=covertype_training_deployment,\
 _RUNTIME_VERSION=1.14,\
 _PYTHON_VERSION=3.5,\
 _COMPONENT_URL_SEARCH_PREFIX=https://raw.githubusercontent.com/kubeflow/pipelines/0.1.38/components/gcp/,\
-_CLUSTER_NAME=mlops-workshop-cluster,\
 _ZONE=us-central1-a
 
 
