a85e61adf55ad2f66c0ab647a86f8ab29d56fee2
jarokaz <jarokaz@users.no-reply.github.com>
2020-01-23 18:34:19 +0000

    Updates to lab 13

--- lab-13-kfp-cicd/build_pipeline.sh
+++ lab-13-kfp-cicd/build_pipeline.sh
@@ -21,13 +21,13 @@ _INVERTING_PROXY_HOST=[YOUR_INVERTING_PROXY_HOST],\
 _TRAINER_IMAGE_NAME=trainer_image,\
 _BASE_IMAGE_NAME=base_image,\
 TAG_NAME=manual,\
-_PIPELINE_FOLDER=Lab-11-KFP-CAIP/pipelines,\
+_PIPELINE_FOLDER=pipeline,\
 _PIPELINE_DSL=covertype_training_pipeline.py,\
 _PIPELINE_PACKAGE=covertype_training_pipeline.yaml,\
 _PIPELINE_NAME=covertype_training_deployment,\
 _RUNTIME_VERSION=1.14,\
 _PYTHON_VERSION=3.5,\
-_COMPONENT_URL_SEARCH_PREFIX=https://raw.githubusercontent.com/kubeflow/pipelines/0.1.38/components/gcp/
+_COMPONENT_URL_SEARCH_PREFIX=https://raw.githubusercontent.com/kubeflow/pipelines/0.1.36/components/gcp/
 
 
 gcloud builds submit ../.. --config cloudbuild.yaml --substitutions $SUBSTITUTIONS
