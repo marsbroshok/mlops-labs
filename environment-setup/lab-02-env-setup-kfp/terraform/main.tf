5e73043dac15e328b18ece3a809d4cf31b865faf
Jarek Kazmierczak <jarokaz@hotmail.com>
2020-02-18 12:43:49 -0800

    Update main.tf

--- environment-setup/lab-02-env-setup-kfp/terraform/main.tf
+++ environment-setup/lab-02-env-setup-kfp/terraform/main.tf
@@ -14,7 +14,7 @@ provider "google" {
 
 # Create the GKE service account 
 module "gke_service_account" {
-  source                       = "../../lab-00-environment-setup/terraform/modules/service_account"
+  source                       = "../../lab-00-env-setup-automated/terraform/modules/service_account"
   service_account_id           = "${var.name_prefix}-gke-sa"
   service_account_display_name = "The GKE service account"
   service_account_roles        = var.gke_service_account_roles
@@ -22,7 +22,7 @@ module "gke_service_account" {
 
 # Create the KFP service account 
 module "kfp_service_account" {
-  source                       = "../../lab-00-environment-setup/terraform/modules/service_account"
+  source                       = "../../lab-00-env-setup-automated/terraform/modules/service_account"
   service_account_id           = "${var.name_prefix}-sa"
   service_account_display_name = "The KFP service account"
   service_account_roles        = var.kfp_service_account_roles
@@ -30,7 +30,7 @@ module "kfp_service_account" {
 
 # Create the VPC for the KFP cluster
 module "kfp_gke_vpc" {
-  source                 = "../../lab-00-environment-setup/terraform/modules/vpc"
+  source                 = "../../lab-00-env-setup-automated/terraform/modules/vpc"
   region                 = var.region
   network_name           = "${var.name_prefix}-network"
   subnet_name            = "${var.name_prefix}-subnet"
@@ -38,7 +38,7 @@ module "kfp_gke_vpc" {
 
 # Create the KFP GKE cluster
 module "kfp_gke_cluster" {
-  source                 = "../../lab-00-environment-setup/terraform/modules/gke"
+  source                 = "../../lab-00-env-setup-automated/terraform/modules/gke"
   name                   = "${var.name_prefix}-cluster"
   location               = var.zone != "" ? var.zone : var.region
   description            = "KFP GKE cluster"
@@ -50,11 +50,11 @@ module "kfp_gke_cluster" {
 }
 
 # Create the MySQL instance for ML Metadata
-module "ml_metadata_mysql" {
-  source  = "../../lab-00-environment-setup/terraform//modules/mysql"
-  region  = var.region
-  name    = "${var.name_prefix}-metadata"
-}
+#module "ml_metadata_mysql" {
+#  source  = "../../lab-00-env-setup-automated/terraform//modules/mysql"
+#  region  = var.region
+#  name    = "${var.name_prefix}-metadata"
+#}
 
 # Create Cloud Storage bucket for artifact storage
 resource "google_storage_bucket" "artifact_store" {
