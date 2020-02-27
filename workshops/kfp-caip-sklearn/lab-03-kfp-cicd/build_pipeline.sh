300a31a2e3536ca3d4ab740525b9c3d2011bb059
jarokaz <jarokaz@no-reply.com>
2020-02-27 05:39:33 +0000

    Updates

--- workshops/kfp-caip-sklearn/lab-03-kfp-cicd/build_pipeline.sh
+++ workshops/kfp-caip-sklearn/lab-03-kfp-cicd/build_pipeline.sh
@@ -17,17 +17,17 @@
 # the pipelines and pipelines components 
 
 SUBSTITUTIONS=\
-_INVERTING_PROXY_HOST=[YOUR_INVERTING_PROXY_HOST],\
+_INVERTING_PROXY_HOST=[YOUR_INVERSE_PROXY],\
 _TRAINER_IMAGE_NAME=trainer_image,\
 _BASE_IMAGE_NAME=base_image,\
 TAG_NAME=test,\
-_PIPELINE_FOLDER=lab-13-kfp-cicd/pipeline,\
+_PIPELINE_FOLDER=lab-03-kfp-cicd/pipeline,\
 _PIPELINE_DSL=covertype_training_pipeline.py,\
 _PIPELINE_PACKAGE=covertype_training_pipeline.yaml,\
 _PIPELINE_NAME=covertype_training_deployment,\
 _RUNTIME_VERSION=1.14,\
 _PYTHON_VERSION=3.5,\
-_COMPONENT_URL_SEARCH_PREFIX=https://raw.githubusercontent.com/kubeflow/pipelines/0.2.2/components/gcp/
+_COMPONENT_URL_SEARCH_PREFIX=https://raw.githubusercontent.com/kubeflow/pipelines/0.2.4/components/gcp/
 
 
 gcloud builds submit .. --config cloudbuild.yaml --substitutions $SUBSTITUTIONS
